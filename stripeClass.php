4d4c03a6756bc9c6462e94f3e4afea1ba07f7629
Khoa Pham <khoa@whitetiger.sg>
2019-02-14 11:41:02 +0700

    connect to stripe check customer by email create new customer

--- stripeClass.php
+++ stripeClass.php
@@ -4,4 +4,25 @@ namespace drupal_ex\stripe_ex;
 
 class stripeClass {
 
+  protected $pub_key, $s_key;
+
+  public function __construct($pub_key, $s_key) {
+    $this->pub_key = $pub_key;
+    $this->s_key = $s_key;
+  }
+
+  public function connectStripe() {
+    \Stripe\Stripe::setApiKey($this->s_key);
+    echo 1;
+  }
+
+  public function checkCustomerByEmail($email = '') {
+    return \Stripe\Customer::all(["limit" => 1, "email" => $email]);
+  }
+
+  public static function createCustomer($data = []) {
+    if (count($data) == 0)
+      return false;
+  }
+
 }
